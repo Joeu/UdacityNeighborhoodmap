d0bcb576f57093a777d9974337c64adb9b0ad1cb
Joeumar Souza <sjoeumar@lenovo.com>
2018-07-19 16:10:49 -0300

    Displaying map

---
+++ app/view/mapView.js
@@ -0,0 +1,8 @@
+var initMap = function(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -7.2290752, lng: -35.8808337},
+    zoom: 13,
+    mapTypeControl: false
+  });
+}
+
